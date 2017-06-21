395ea547f60f16b97cb946641daa0cd62ba89d76
rista404 <risticn6@gmail.com>
2017-06-21 18:52:00 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const facts = [
+	{
+		fact: 'Earth is the only planet with life on it (that we know of).',
+		photo: 'http://apod.nasa.gov/apod/image/1112/IMG_1552ribeiro900.jpg',
+	},
+	{
+		fact: 'Mercury, Venus, Earth, Mars, and Jupiter have no rings.',
+		photo: 'http://apod.nasa.gov/apod/image/1112/zagorje-2Boris900.jpg',
+	},
+	{
+		fact: 'Saturn has the brightest rings.',
+		photo: 'http://apod.nasa.gov/apod/image/1112/aurora2_salomonsen_900h.jpg',
+	},
+	{
+		fact: 'Uranus and Neptune have rings, but they are dim.',
+		photo: 'http://apod.nasa.gov/apod/image/1112/sh2_239block900c.jpg',
+	},
+]
